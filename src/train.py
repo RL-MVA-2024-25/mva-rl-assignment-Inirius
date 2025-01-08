055c104cd8f08b7c1a135c213272d14af8ba71b3
Auguste De Lambilly <auguste.delam@yahoo.fr>
2025-01-08 21:34:29 +0100

    trained

--- src/train.py
+++ src/train.py
@@ -8,10 +8,9 @@ import random
 from copy import deepcopy
 import os
 import argparse
-from fast_env import FastHIVPatient
 
 env = TimeLimit(
-    env=FastHIVPatient(domain_randomization=False), max_episode_steps=200
+    env=HIVPatient(domain_randomization=False), max_episode_steps=200
 )  # The time wrapper limits the number of steps in an episode at 200.
 # Now is the floor is yours to implement the agent and train it.
 
